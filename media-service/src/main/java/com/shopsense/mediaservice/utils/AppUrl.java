f81a8cd6dd3b5b6e74d24357fc3089d265150770
Vignesh M <vigneshmmuthusamy@gmail.com>
2026-09-03 17:55:57 +0530

    Changed the api base url for the cloudinary

--- media-service/src/main/java/com/shopsense/mediaservice/utils/AppUrl.java
+++ media-service/src/main/java/com/shopsense/mediaservice/utils/AppUrl.java
@@ -4,8 +4,7 @@ public class AppUrl {
 
     private AppUrl(){}
 
-    private static final String BASE_URL = "/api/v1/media";
+    private static final String BASE_URL = "/api/v1";
 
     public static final String CLOUDINARY_MEDIA_URL = BASE_URL + "/cloudinary";
-    public static final String AWS_URL = BASE_URL + "/aws";
 }
