2c809185e7fd400ad5af0248daaf739365430262
Vignesh M <vigneshmmuthusamy@gmail.com>
2026-09-14 08:27:26 +0530

    Moved hardcoded properties to env file
    Moved all the properties to the env file

--- media-service/src/main/java/com/shopsense/mediaservice/config/CloudinaryConfig.java
+++ media-service/src/main/java/com/shopsense/mediaservice/config/CloudinaryConfig.java
@@ -3,12 +3,10 @@ package com.shopsense.mediaservice.config;
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@RefreshScope
 public class CloudinaryConfig {
 
     @Value("${cloudinary.cloud-name}")
