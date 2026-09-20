3f3cc65c78d992134160c61160fdaf513a5baaf5
Vignesh M <vigneshmmuthusamy@gmail.com>
2026-09-20 18:09:24 +0530

    Added client urls

--- user-service/src/main/java/com/shopsense/userservice/utils/AppUrl.java
+++ user-service/src/main/java/com/shopsense/userservice/utils/AppUrl.java
@@ -4,12 +4,16 @@ public class AppUrl {
 
     private AppUrl(){}
 
+    //    Base URL
     private static final String BASE_URL = "/api/v1";
 
+    //    Endpoints for this service
     public static final String CUSTOMER_URL = BASE_URL + "/customers";
     public static final String SELLER_URL = BASE_URL + "/sellers";
     public static final String ADMIN_URL = BASE_URL + "/admins";
     public static final String ADDRESS_URL = BASE_URL + "/addresses";
+
+    //    Endpoints for Http Clients
     public static final String MEDIA_URL = BASE_URL + "/media";
 
 }
