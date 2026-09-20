27c74180ef30971444d62530f07a2570eb868194
Vignesh M <vigneshmmuthusamy@gmail.com>
2026-09-20 18:09:47 +0530

    Added client urls

--- media-service/src/main/java/com/shopsense/mediaservice/utils/AppUrl.java
+++ media-service/src/main/java/com/shopsense/mediaservice/utils/AppUrl.java
@@ -4,7 +4,9 @@ public class AppUrl {
 
     private AppUrl(){}
 
+//    Base URL
     private static final String BASE_URL = "/api/v1";
 
+    //    Endpoints for this service
     public static final String MEDIA_URL = BASE_URL + "/media";
 }
