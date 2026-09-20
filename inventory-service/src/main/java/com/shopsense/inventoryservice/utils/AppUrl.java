4c43c5e379e71bed1fdaf059ba885d689c29af2c
Vignesh M <vigneshmmuthusamy@gmail.com>
2026-09-20 18:09:15 +0530

    Added client urls

--- inventory-service/src/main/java/com/shopsense/inventoryservice/utils/AppUrl.java
+++ inventory-service/src/main/java/com/shopsense/inventoryservice/utils/AppUrl.java
@@ -4,10 +4,14 @@ public class AppUrl {
 
     private AppUrl(){}
 
+    //    Base URL
     private static final String BASE_URL = "/api/v1";
 
+    //    Endpoints for this service
     public static final String INVENTORY_URL  = BASE_URL + "/inventory";
     public static final String RESERVATION_URL  = BASE_URL + "/reservations";
+
+    //    Endpoints for Http Clients
     public static final String PRODUCT_URL = BASE_URL + "/products";
 
 }
